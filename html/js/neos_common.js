60c64d406ce0f8bcdeec6ae3c3588d0733e3c43c
Sinduy <sjsanjsrh@naver.com>
2022-12-16 23:04:40 +0900

    add (optional) parent tag name

--- html/js/neos_common.js
+++ html/js/neos_common.js
@@ -2,18 +2,20 @@
  * script for neos fomat to html fomat
  * @file neos_common.js
  * @author Sinduy <sjsanjsrh@naver.com>
- * @version 1.0.0
+ * @version 1.0.1
  */
 
 /**
  * convert neos text to html text
  * @param {string} text neos text
+ * @param {string} tag (optional) parent tag name
  * @returns {string} html text
  */
-function neos_text_to_html(text) {
+function neos_text_to_html(text, tag) {
     let res = new String(text)
     // change color tag
     res = res.replace(/<color=(.*?)>/g, '<span style="color:$1">');
     res = res.replace(/<\/color>/g, '</span>');
-    return res;
+    if(tag) return "<"+tag+">"+res+"</"+tag+">";
+    else return res;
 }
