dd70818816e91079157b46680ccdbc659e6f0771
Sinduy <sjsanjsrh@naver.com>
2023-01-02 23:31:33 +0900

    remove unused config

--- ConfigLoad.js
+++ ConfigLoad.js
@@ -9,7 +9,6 @@ const configPath = path.join(__dirname, 'config.json');
 const fs = require('fs');
 
 const defaultConfig = {
-    password: "",
     express: false,
     express_port: 8080,
     thumbnail_dir: "html/thumbnails/",
@@ -29,9 +28,16 @@ function loadConfig(path) {
 }
 
 const config = loadConfig(configPath);
+
 for (let key in defaultConfig) {
     if (!config.hasOwnProperty(key)) {
         config[key] = defaultConfig[key];
+    }
+}
+
+for(let key in config) {
+    if(!defaultConfig.hasOwnProperty(key)) {
+        delete config[key];
     }
 }
 
